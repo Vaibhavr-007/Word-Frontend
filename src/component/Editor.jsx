30c4bc87258d64b033b596ece054614ba3e01866
vaibhav <90847434+Vaibhavr-007@users.noreply.github.com>
2023-05-23 15:51:17 +0530

    Add files via upload

---
+++ src/component/Editor.jsx
@@ -0,0 +1,130 @@
+import { useEffect , useState , useCallback,useRef} from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+import { Box } from '@mui/material';
+import styled from '@emotion/styled';
+import { useParams } from 'react-router-dom';
+import {io} from 'socket.io-client';
+const Component = styled.div`
+  background: #F5F5F5;
+`;
+
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
+const Editor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  const { id } = useParams(); // property of react-router-dom
+  // useEffect(() => {
+  //   const quillServer = new Quill('#container', { theme: 'snow', modules: { toolbar: toolbarOptions }});
+  //   quillServer.disable();
+  //   quillServer.setText('Loading the document...')
+  //   setQuill(quillServer);
+  // }, []);
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.appendChild(editor);
+    const quillServer = new Quill(editor, { theme: 'snow', modules: { toolbar: toolbarOptions } });
+    quillServer.disable();
+    quillServer.setText('Loading the document...')
+    setQuill(quillServer);
+  }, []);
+  
+
+
+  useEffect(() => {
+    const socketServer = io('https://wordbackend.onrender.com');
+    setSocket(socketServer);
+
+    return () => {
+        socketServer.disconnect();
+    }
+  }, [])
+
+    useEffect(() => {
+    if (socket === null || quill === null) return;
+
+    const handleChange = (delta, oldData, source) => {
+        if (source !== 'user') return;
+
+        socket.emit('send-changes', delta);
+    }
+
+    quill && quill.on('text-change', handleChange);
+
+    return () => {
+        quill && quill.off('text-change', handleChange);
+    }
+  }, [quill, socket])
+
+
+  useEffect(() => {
+    if (socket === null || quill === null) return;
+
+    const handleChange = (delta) => {
+        quill.updateContents(delta);
+
+    }
+    socket && socket.on('receive-changes', handleChange);
+
+    return () => {
+        socket && socket.off('receive-changes', handleChange);
+    }
+  }, [quill, socket]);
+
+
+
+  // for id
+
+  useEffect(() => {
+    if (quill === null || socket === null) return;
+
+    socket && socket.once('load-document', document => {
+        quill.setContents(document);
+        quill.enable();
+    })
+
+    socket && socket.emit('get-document', id);
+  },  [quill, socket, id]);
+
+
+  useEffect(()=>{
+    if(socket===null || quill===null)return;
+
+    const interval=setInterval(()=>{
+      socket && socket.emit('save-document',quill.getContents());
+    },2000)
+
+    return ()=>{
+      clearInterval(interval);
+    }
+  },[socket,quill]);
+
+  return (
+    <Component>
+      <Box className="container" id="container" ref={wrapperRef}></Box>
+    </Component>
+  );
+};
+
+export default Editor;
